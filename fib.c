959ce973e61ae1ac574eb1c85f944b0fb3e77c99
Manni2468 <manoj.rajgopal.kumar@gmail.com>
2023-12-29 19:09:31 +0000

    added fic.c

---
+++ fib.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fib() {
+    int num, i, first = 0, second = 1, next;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &num);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 1; i <= num; ++i) {
+        if (i == 1) {
+            printf("%d, ", first);
+            continue;
+        }
+        if (i == 2) {
+            printf("%d, ", second);
+            continue;
+        }
+        next = first + second;
+        first = second;
+        second = next;
+        printf("%d, ", next);
+	printf("\n\n");
+    }
+
+    return 0;
+}
